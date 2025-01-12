fedd68220803c60bcb8d44873a819002cf53fe3d
Enoch Tetteh <cutetetteh@gmail.com>
2025-01-11 21:04:49 -0500

    Remove redundant code

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import node from '@astrojs/node';
 import tailwind from '@astrojs/tailwind';
 import alpinejs from '@astrojs/alpinejs';
 import react from '@astrojs/react';
